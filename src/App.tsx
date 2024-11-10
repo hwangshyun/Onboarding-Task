11a61f90aa2d9b4a3e599fd8b8be215d2a99f4d3
hwangseunghyun <hwamg123@naver.com>
2024-11-11 00:23:47 +0900

    tailwind 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,14 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        {/* 메인 페이지 (로그인 여부와 상관없이 접근 가능) */}
         <Route path="/" element={<Home />} />
-
-        {/* PublicRoute를 사용하여 로그인된 사용자가 접근할 수 없도록 설정 */}
         <Route element={<PublicRoute />}>
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
         </Route>
-
-        {/* PrivateRoute를 사용하여 로그인된 사용자만 접근 가능하도록 설정 */}
         <Route element={<PrivateRoute />}>
           <Route path="/mypage" element={<Mypage />} />
         </Route>
-
-        {/* 모든 다른 경로는 메인 페이지로 리다이렉트 */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      css: {
-        additionalData: `@import "./src/index.css";`
-      }
-    }
-  },
   server: {
     proxy: {
       '/api': {
